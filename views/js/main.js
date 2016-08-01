0f15dea6c1c6b24ea633f3684b644def5423f49f
Gabe Gonzales <grmgonz@gmail.com>
2016-08-01 15:08:37 -0600

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -504,9 +504,11 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
+  var halfScreenWidth = ((window.innerWidth > 0) ? window.innerWidth : screen.width) / 2; 
+  
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  items[i].style.transform = 'translateX('+(items[i].basicLeft + 100 * phase - halfScreenWidth) + 'px)';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
